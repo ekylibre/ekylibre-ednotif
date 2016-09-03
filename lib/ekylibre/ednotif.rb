b7659381986cd765076cbd8cde1dd0445eeef643
Nicolas PROCUREUR <nprocureur@gmail.com>
2016-09-03 19:09:15 +0200

    Hooks in interventions

--- lib/ekylibre/ednotif.rb
+++ lib/ekylibre/ednotif.rb
@@ -1,5 +1,6 @@
 module Ekylibre
   module Ednotif
+    EDNOTIF_VERSION = 1
     #in/ : from Ednotif to Ekylibre transcoding
     #out/ : from Ekylibre to Ednotif transcoding
 
@@ -24,6 +25,10 @@ module Ekylibre
         root.join('resources')
       end
 
+      def schema_definition
+        import_dir.join('IpBNotif_v1.xsd') if EDNOTIF_VERSION == 1
+      end
+
       def schema_url
         'http://idele.fr/XML/Schema/'.freeze
       end
@@ -36,6 +41,38 @@ module Ekylibre
         transcoding_dir.join('routines.yml')
       end
     end
+
+    ACTIONS = {
+        parturition: :create_new_birth,
+        animal_artificial_insemination: :create_insemination
+    }
+
+    def self.included(base)
+      # base.send :helper_method, :ednotify? if base.respond_to? :helper_method
+
+      base.instance_eval do
+        after_create :notify
+      end
+
+      base.class_eval do
+
+        def ednotify?
+          true
+        end
+
+        def notify
+
+          if self.is_a? Intervention and nature == 'record'
+            actions.each do |action|
+              next unless  ACTIONS.key? action
+              ::Ekylibre::Ednotif::ServiceCaller.send ACTIONS[:action], self
+            end
+          elsif self.is_a? Purchase
+
+          end
+          true
+        end
+      end
+    end
   end
 end
-
