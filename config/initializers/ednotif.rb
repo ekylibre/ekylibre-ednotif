58fd259cd09e3196906b12f736def5a7301ad14c
Nicolas PROCUREUR <nprocureur@gmail.com>
2016-09-09 18:50:52 +0200

    Load transcoding tables

--- config/initializers/ednotif.rb
+++ config/initializers/ednotif.rb
@@ -2,3 +2,17 @@
 require 'ekylibre/ednotif'
 
 autoload :Ednotif, 'ednotif'
+
+# Ednotif::EdnotifIntegration.on_check_success do
+  # Ednotif::EdnotifGetListJob.perform_later
+# end
+
+#hook : subscribe :new_birth,
+
+
+# Loads transcoding tables
+files = Dir.glob(Ekylibre::Ednotif.in_transcoding_dir.join('*')) - Dir.glob(Ekylibre::Ednotif.in_transcoding_dir.join('*.exception.yml'))
+YamlNomen.load(:incoming, files)
+
+files = Dir.glob(Ekylibre::Ednotif.out_transcoding_dir.join('*')) - Dir.glob(Ekylibre::Ednotif.out_transcoding_dir.join('*.exception.yml'))
+YamlNomen.load(:outgoing, files)
