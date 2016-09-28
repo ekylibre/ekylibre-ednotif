4b643608a10c8c3a017143a3ce3870fb50bb60f4
Nicolas PROCUREUR <nprocureur@gmail.com>
2016-09-28 18:48:46 +0200

    Adds ednotif constants

--- lib/ednotif.rb
+++ lib/ednotif.rb
@@ -1,4 +1,13 @@
 module Ednotif
+  EDNOTIF_VERSION = '1.00'
+  DIRECTORY_WSDL = 'http://ws-directory.fiea.fr/wsannuaire/WsAnnuaire?wsdl'.freeze
+  TEST_DIRECTORY_WSDL = 'http://wstest-directory.fiea.fr/wsannuaire/WsAnnuaire?wsdl'.freeze
+
+  # 9 is for national webservices
+  CODE_SITE_VERSION = 9
+  SERVICE_NAME = 'IpBNotif'
+  APPLICATION_LABEL = 'Ekylibre'
+
   class << self
     def build
 
