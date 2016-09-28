11577f9f47b82766299288b5a3d725ee7d54f410
Nicolas PROCUREUR <nprocureur@gmail.com>
2016-09-28 18:51:10 +0200

    Updates ednotif get list job

--- app/jobs/ednotif/ednotif_get_list_job.rb
+++ app/jobs/ednotif/ednotif_get_list_job.rb
@@ -1,16 +1,47 @@
 module Ednotif
-  class EdnotifGetListJob < ActiveJob::Bse
+  class EdnotifGetListJob < ActiveJob::Base
     queue_as :default
 
     def perform(*args)
-      Ednotif::EdnotifIntegration.get_list(*args).execute do |c|
-        #TODO
-        # get ekylibre transcoded herd list.
-      end
+      operation_name = :get_list.freeze
+      dataset = args.extract_options!
 
-      # c.on :animal_is_already_registered do
-      #   #some stuff
-      # end
+      farm_number = Identifier.where(nature: :cattling_number)
+
+      dataset[:farm_number] = farm_number.first[:value][2..10] unless farm_number.empty?
+      dataset[:start_date] ||= Date.today.to_s
+      dataset[:get_stock] ||= false
+
+
+      ### validations
+
+      #tmp
+      fail 'farm number empty' unless farm_number.present?
+      fail 'invalid start date' unless Date.parse(dataset[:start_date]) <= Date.today
+      fail 'end date must be superior to start date' unless dataset[:end_date].blank? or Date.parse(dataset[:start_date]) <= Date.parse(dataset[:end_date])
+      fail 'invalid get stock value' unless [true, false].include? dataset[:get_stock]
+
+      message = {
+          token: dataset[:token],
+          farm: {
+              country_code: dataset[:country_code],
+              farm_number: dataset[:farm_number]
+          },
+          start_date: dataset[:start_date],
+          stock: dataset[:get_stock]
+      }
+
+      message.merge!(end_date: dataset[:end_date]) if dataset.key? :end_date
+
+      logger = EdnotifLogger.create!(operation_name: operation_name)
+
+      Ednotif::EdnotifIntegration.authenticate_and_do logger do
+        # TODO message
+        # Ednotif::EdnotifIntegration.get_list(options: options, message: message).execute do |c|
+        #   c.success do |response|
+        #   end
+        # end
+      end
     end
   end
 end
