057cab443c1a3a1c37d0a77a1979bf5feff2c015
Nicolas PROCUREUR <nprocureur@gmail.com>
2016-09-30 17:36:42 +0200

    Updates GetListJob to call get_list operation.

--- app/jobs/ednotif/ednotif_get_list_job.rb
+++ app/jobs/ednotif/ednotif_get_list_job.rb
@@ -4,43 +4,54 @@ module Ednotif
 
     def perform(*args)
       operation_name = :get_list.freeze
+      logger = EdnotifLogger.create!(operation_name: operation_name)
+
       dataset = args.extract_options!
 
-      farm_number = Identifier.where(nature: :cattling_number)
+      # TODO: re-enable after testing
+      # dataset[:farm_number] = Identifier.where(nature: :cattling_number).first[:value]
+      dataset[:farm_number] = 'FR01999999'
 
-      dataset[:farm_number] = farm_number.first[:value][2..10] unless farm_number.empty?
-      dataset[:start_date] ||= Date.today.to_s
+      dataset[:start_date] ||= Date.today
       dataset[:get_stock] ||= false
 
 
       ### validations
 
       #tmp
-      fail 'farm number empty' unless farm_number.present?
-      fail 'invalid start date' unless Date.parse(dataset[:start_date]) <= Date.today
-      fail 'end date must be superior to start date' unless dataset[:end_date].blank? or Date.parse(dataset[:start_date]) <= Date.parse(dataset[:end_date])
+      fail 'farm number is empty' unless dataset[:farm_number].present? and dataset[:farm_number] =~ /[A-Za-z]{2}[0-9]{8}/
+      fail 'invalid start date' unless dataset[:start_date] <= Date.today
+      fail 'end date must be superior to start date' unless dataset[:end_date].blank? or dataset[:start_date] <= dataset[:end_date]
       fail 'invalid get stock value' unless [true, false].include? dataset[:get_stock]
 
-      message = {
-          token: dataset[:token],
-          farm: {
-              country_code: dataset[:country_code],
-              farm_number: dataset[:farm_number]
-          },
-          start_date: dataset[:start_date],
-          stock: dataset[:get_stock]
-      }
-
-      message.merge!(end_date: dataset[:end_date]) if dataset.key? :end_date
-
-      logger = EdnotifLogger.create!(operation_name: operation_name)
 
       Ednotif::EdnotifIntegration.authenticate_and_do logger do
-        # TODO message
-        # Ednotif::EdnotifIntegration.get_list(options: options, message: message).execute do |c|
-        #   c.success do |response|
-        #   end
-        # end
+        integration ||= Ednotif::EdnotifIntegration.fetch
+
+        options = {
+            globals: {
+                wsdl: integration.parameters['business_wsdl']
+            }
+        }
+
+        # remember to observe xsd sequences.
+        message = {
+            token: integration.parameters['token'],
+            farm: {
+                farm_number: dataset[:farm_number]
+            },
+            start_date: dataset[:start_date],
+            end_date: dataset.key?(:end_date) ? dataset[:end_date] : nil,
+            stock: dataset[:get_stock]
+        }
+
+        Ednotif::EdnotifIntegration.get_list(options: options, message: message).execute(logger) do |op_c|
+          op_c.success do |op_response|
+            op_response
+            binding.pry
+            # work with ekylibre hash
+          end
+        end
       end
     end
   end
