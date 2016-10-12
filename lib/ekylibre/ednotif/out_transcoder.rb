b67174b89589fe8c15495b1a9dc9d580fae11a17
Nicolas PROCUREUR <nprocureur@gmail.com>
2016-10-12 18:23:01 +0200

    Updates transcoder and error codes.

--- lib/ekylibre/ednotif/out_transcoder.rb
+++ lib/ekylibre/ednotif/out_transcoder.rb
@@ -47,6 +47,12 @@ module Ekylibre
           }
         end
 
+        def origin_farm(fragment)
+          {
+              'edn:ExploitationProvenance': convert(fragment)
+          }
+        end
+
         def animal(fragment)
           {
               'edn:Bovin': convert(fragment)
@@ -73,19 +79,28 @@ module Ekylibre
 
         #terminals:
 
-        def passport_request(fragment)
+        def passport_ask(fragment)
           {
-              'edn:DemandePasseport': fragment
+              'edn:DemandePasseport': true & fragment ? 1 : 0
           }
         end
 
-        def work_code(fragment)
+        def prod_code(fragment)
+          return :invalid_prod_code unless fragment.present?
           {
-              'edn:CodeAtelier': YamlNomen[:outgoing][:work_code][fragment]
+              'edn:CodeAtelier': fragment
+          }
+        end
+
+        def cattle_categ_code(fragment)
+          return :invalid_cattle_categ_code unless fragment.present?
+          {
+              'edn:CodeCategorieBovin': fragment
           }
         end
 
         def birth_weight(fragment)
+          return :invalid_birth_weight unless fragment.to_s.to_i <= 99
           {
               'edn:PoidsNaissance': fragment
           }
@@ -93,29 +108,31 @@ module Ekylibre
 
         def weighed_weight(fragment)
           {
-              'edn:PoidsPese': fragment
+              'edn:PoidsPese': true & fragment ? 1 : 0
           }
         end
 
         def chest_size(fragment)
+          return :invalid_chest_size unless fragment.to_s.to_i <= 999
           {
               'edn:TourPoitrine': fragment
           }
         end
 
-        def transplantation(fragment)
+        def transplant(fragment)
           {
-              'edn:TransplantationEmbryonnaire': fragment
+              'edn:TransplantationEmbryonnaire': true & fragment ? 1 : 0
           }
         end
 
         def abortion(fragment)
           {
-              'edn:Avortement': fragment
+              'edn:Avortement': true & fragment ? 1 : 0
           }
         end
 
         def birth_condition(fragment)
+          return :invalid_birth_condition if YamlNomen[:outgoing][:mammalia_birth_conditions][fragment].nil?
           {
               'edn:ConditionNaissance': YamlNomen[:outgoing][:mammalia_birth_conditions][fragment]
           }
@@ -123,89 +140,104 @@ module Ekylibre
 
         def twin(fragment)
           {
-              'edn:Jumeau': fragment
+              'edn:Jumeau': true & fragment ? 1 : 0
           }
         end
 
         def token(fragment)
+          return :invalid_token unless fragment.present?
           {
               'edn:JetonAuthentification': fragment
           }
         end
 
         def login(fragment)
+          return :invalid_login unless fragment.present?
           {
               'typ:UserId': fragment
           }
         end
 
         def password(fragment)
+          return :invalid_password unless fragment.present?
           {
               'typ:Password': fragment
           }
         end
 
         def enterprise(fragment)
+          return :invalid_enterprise unless fragment.present?
           {
               'typ:Entreprise': fragment
           }
         end
 
         def application(fragment)
+          return :invalid_application_name unless fragment.present?
           {
               'typ:Application': fragment
           }
         end
 
         def version_number(fragment)
+          return :invalid_version_number unless fragment.present?
           {
               'typ:NumeroVersion': fragment
           }
         end
 
         def site_code_version(fragment)
+          return :invalid_site_code_version unless fragment.present?
           {
               'typ:CodeSiteVersion': fragment
           }
         end
 
         def service_name(fragment)
+          return :invalid_service_name unless fragment.present?
           {
               'typ:NomService': fragment
           }
         end
 
         def site_code_service(fragment)
+          return :invalid_site_code_service unless fragment.present?
           {
               'typ:CodeSiteService': fragment
           }
         end
 
         def country_code(fragment)
+          return :invalid_country_code unless fragment =~ /[a-z]{2}/
           {
               'edn:CodePays': YamlNomen[:outgoing][:countries][fragment]
           }
         end
 
         def identification_number(fragment)
+          # return :invalid_identification_number unless fragment =~ /[A-Za-z0-9]{10,12}/
+          return :invalid_identification_number unless fragment =~ /[A-Za-z]{2}[0-9]{10}/
           {
               'edn:NumeroNational': fragment
-          }
+          }.reverse_merge(country_code(fragment[0..1].downcase))
         end
 
         def sex(fragment)
+          return :invalid_sex if YamlNomen[:outgoing][:sexes][fragment].nil?
           {
               'edn:Sexe': YamlNomen[:outgoing][:sexes][fragment]
           }
         end
 
         def race_code(fragment)
+          return :invalid_race_code if YamlNomen[:outgoing]['varieties-bos_taurus'][fragment].nil?
           {
               'edn:TypeRacial': YamlNomen[:outgoing]['varieties-bos_taurus'][fragment]
           }
         end
 
         def birth_date(fragment)
+          return :invalid_birth_date unless fragment.class.to_s == 'Date'
           # as a DateTime object
           {
               'edn:DateNaissance': fragment.to_s
@@ -213,30 +245,43 @@ module Ekylibre
         end
 
         def work_number(fragment)
+          return :invalid_work_number unless fragment.present?
           {
               'edn:NumeroTravail': fragment
           }
         end
 
         def name(fragment)
+          return :invalid_name unless fragment.present?
           {
               'edn:NomBovin': fragment
           }
         end
 
         def farm_number(fragment)
+          return :invalid_farm_number unless fragment =~ /[A-Za-z]{2}[0-9]{8}/
           {
               'edn:NumeroExploitation': fragment[2..10]
           }.reverse_merge(country_code(fragment[0..1].downcase))
         end
 
+
+        def origin_owner_name(fragment)
+          return :invalid_origin_owner_name unless fragment.present?
+          {
+              'edn:NomExploitation': fragment
+          }
+        end
+
         def start_date(fragment)
+          return :invalid_start_date unless fragment.class.to_s == 'Date'
           {
               'edn:DateDebut': fragment.to_s
           }
         end
 
         def end_date(fragment)
+          return :invalid_end_date unless fragment.class.to_s == 'Date'
           {
               'edn:DateFin': fragment.to_s
           }
@@ -248,13 +293,45 @@ module Ekylibre
           }
         end
 
-        def convert(message)
-          message.each_with_object({}) do |(k, v), h|
-            h.merge!(send(k, v).reject{ |_,v| v.blank? })
-          end
+        def entry_date(fragment)
+          return :invalid_entry_date unless fragment.class.to_s == 'Date'
+          {
+              'edn:DateEntree': fragment.to_s
+          }
+        end
+
+        def entry_reason(fragment)
+          return :invalid_entry_reason if YamlNomen[:outgoing][:entry_reason][fragment].nil?
+          {
+              'edn:CauseEntree': YamlNomen[:outgoing][:entry_reason][fragment]
+          }
         end
 
       end
+
+      attr_reader :errors
+
+      def initialize(message)
+        @errors = {}
+        @message = convert message
+      end
+
+      def valid?
+        @errors.blank?
+      end
+
+      def convert(hash)
+        hash.each_with_object({}) do |(k, v), h|
+          message = self.class.send(k, v)
+          if message.is_a? Hash
+            message.reject { |_, v| v.blank? }
+            h.merge!(message)
+          else
+            @errors[message] = v.to_s
+          end
+        end
+      end
+
     end
   end
 end
