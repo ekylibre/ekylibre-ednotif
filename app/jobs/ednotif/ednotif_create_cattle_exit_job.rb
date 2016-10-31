899be4cbe357021227be954c5a8c1de512c07328
Nicolas PROCUREUR <nprocureur@gmail.com>
2016-10-31 14:22:15 +0100

    Create cattle exit operation

--- app/jobs/ednotif/ednotif_create_cattle_exit_job.rb
+++ app/jobs/ednotif/ednotif_create_cattle_exit_job.rb
@@ -13,7 +13,6 @@ module Ednotif
       dataset[:farm_number] = 'FR01999999'
 
 
-
       Ednotif::EdnotifIntegration.authenticate_and_do logger do
         integration ||= Ednotif::EdnotifIntegration.fetch
 
@@ -30,9 +29,10 @@ module Ednotif
                 farm_number: dataset[:farm_number]
             },
             animal:
-            {
-               identification_number: dataset[:identification_number]
-            },
+                {
+                    country_code: dataset[:country_code],
+                    identification_number: dataset[:identification_number]
+                },
             exit_date: dataset[:exit_date],
             exit_reason: dataset[:exit_reason],
             destination_farm: {
@@ -49,31 +49,27 @@ module Ednotif
         end
         message.delete_if(&p)
 
-
-        call = Ednotif::EdnotifIntegration.create_cattle_exit(options: options, message: message)
-
-        call.execute(logger) do |op_c|
+        Ednotif::EdnotifIntegration.create_cattle_exit(options: options, message: message).execute(logger) do |op_c|
           op_c.success do |op_response|
-            binding.pry
-            op_response
-            #TODO
-=begin
-            {:standard_response=>{:result=>true},
-            :particular_response=>
-                {:identity=>
-                     {:country_code=>"fr",
-                      :identification_number=>"0107003537",
-                      :sex=>"female",
-                      :race_code=>"bos_taurus_montbeliarde",
-                      :birth_date=>{:date=>Fri, 21 Sep 2007, :witness=>"full_date"},
-                :work_number=>"3537",
-                :name=>"CALIFORNIE",
-                :mother=>{:country_code=>"fr", :identification_number=>"0102002659", :race_code=>"bos_taurus_montbeliarde"},
-                :father=>{:country_code=>"fr", :identification_number=>"0199068825", :race_code=>"bos_taurus_montbeliarde"},
-                :farm_number=>"01157053"},
-                :entry_mouvement=>{:entry_date=>Thu, 18 Oct 2007, :entry_reason=>"purchase"},
-                :exit_mouvement=>{:exit_date=>Wed, 19 Oct 2016, :exit_reason=>"sale"}}}
-=end
+
+            ActiveRecord::Base.transaction do
+              begin
+                animal = op_response[:particular_response]
+                identity = animal[:identity]
+
+                record = Animal.find_by(identification_number: identity[:identification_number])
+
+                if record and animal[:exit_mouvement] and animal[:exit_mouvement].key? :exit_date
+                  record.read!(:exit_date, animal[:exit_mouvement][:exit_date], at: animal[:exit_mouvement][:exit_date], force: true) unless animal[:exit_mouvement][:exit_date].nil?
+                  record.read!(:exit_reason, animal[:exit_mouvement][:exit_reason], at: animal[:exit_mouvement][:exit_date], force: true) unless animal[:exit_mouvement][:exit_reason].nil?
+                end
+
+              rescue => e
+                #TODO: format errors
+                logger.state = [identity[:identification_number], e.record.errors.messages].to_s if e.respond_to?(:record)
+                logger.save!
+              end
+            end
           end
           # if transcoding error occurs on OutTranscoder
           op_c.error :transcoding_error do |op_response|
