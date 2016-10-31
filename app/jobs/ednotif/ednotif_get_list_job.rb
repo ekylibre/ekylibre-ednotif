44ca8b418237d7630cc04a622092e6313e5c04dc
Nicolas PROCUREUR <nprocureur@gmail.com>
2016-10-31 12:26:51 +0100

    Import animal inventories.

--- app/jobs/ednotif/ednotif_get_list_job.rb
+++ app/jobs/ednotif/ednotif_get_list_job.rb
@@ -51,75 +51,93 @@ module Ednotif
             op_response.fetch(:animals, []).each do |animal|
               identity = animal[:identity]
 
-              begin
-                # 2nd level error handling. Should not occurs as values are checked during transcoding.
-                ActiveRecord::Base.transaction do
+              ActiveRecord::Base.transaction do
+                begin
 
                   item = Nomen::Variety[identity[:race_code]]
                   variety = (item ? item.name : :bos_taurus)
 
                   attrs = {
-                    identification_number: identity[:identification_number],
-                    work_number: identity[:work_number],
-                    name: identity[:name],
-                    variety: variety,
-                    initial_population: 1.0,
-                    initial_born_at: (identity[:birth_date] ? identity[:birth_date][:date] : nil),
-                    initial_dead_at: (identity[:end_of_life] ? identity[:end_of_life][:end_of_life_date] : nil),
-                    owner: Entity.of_company,
+                      identification_number: identity[:identification_number],
+                      work_number: identity[:work_number].present? ? identity[:work_number] : identity[:identification_number][-4..-1],
+                      name: identity[:name],
+                      variety: variety,
+                      initial_population: 1.0,
+                      initial_born_at: (identity[:birth_date] ? identity[:birth_date][:date] : nil),
+                      initial_dead_at: (identity[:end_of_life] ? identity[:end_of_life][:end_of_life_date] : nil),
+                      owner: Entity.of_company,
                   }
+
+                  # fallbacks
+                  identity[:birth_date] ||= {}
+                  identity[:mother] ||= {}
+                  identity[:father] ||= {}
+                  identity[:sex] ||= :female
+
+
+                  #TODO enhance
+                  attrs[:initial_born_at] = animal[:mouvements].collect { |mvt| mvt[:entry][:entry_date] }.first if attrs[:initial_born_at].blank? and animal.try(:[], :mouvements).collect { |mvt| mvt.try(:[], :entry).try(:[], :entry_date) }.compact.present?
 
                   # 36 months
                   variant = ProductNatureVariant.import_from_nomenclature(((op_response[:generation_datetime].to_date - attrs[:initial_born_at]).to_i > 365*3) ? "#{identity[:sex]}_adult_cow" : "#{identity[:sex]}_young_cow")
                   attrs.merge!({variant: variant})
 
-                  #TODO
-                  # storage, memberships
-
 
                   record = Animal.where(identification_number: identity[:identification_number]).first_or_create!(attrs)
-                  record.read!(:healthy, true, at: attrs[:initial_born_at])
-
-                  record.read!(:witness, identity[:birth_date][:witness], at: attrs[:initial_born_at], force: true) unless identity[:birth_date][:witness].nil?
-                  record.read!(:cpb_filiation_status, identity[:cpb_filiation_status], at: attrs[:initial_born_at], force: true) unless identity[:cpb_filiation_status].nil?
-                  record.read!(:birth_date, identity[:birth_date][:date], at: attrs[:initial_born_at], force: true) unless identity[:birth_date][:date].nil?
-                  record.read!(:birth_farm_number, identity[:farm_number], at: attrs[:initial_born_at], force: true) unless identity[:farm_number].nil?
-                  record.read!(:first_calving_date, identity[:first_calving_date], at: attrs[:initial_born_at], force: true) unless identity[:first_calving_date].nil?
-                  record.read!(:origin_country_code, identity[:origin_country_code], at: attrs[:initial_born_at], force: true) unless identity[:origin_country_code].nil?
-                  record.read!(:origin_identification_number, identity[:origin_identification_number], at: attrs[:initial_born_at], force: true) unless identity[:origin_identification_number].nil?
-                  record.read!(:end_of_life_witness, identity[:end_of_life_witness], at: attrs[:initial_born_at], force: true) unless identity[:end_of_life_witness].nil?
-                  record.read!(:country_code, identity[:country_code], at: attrs[:initial_born_at], force: true) unless identity[:country_code].nil?
-
-                  #mother
-                   record.read!(:mother_country_code, identity[:mother][:country_code], force: true) unless identity[:mother][:country_code].nil?
-                   record.read!(:mother_identification_number, identity[:mother][:identification_number], force: true) unless identity[:mother][:identification_number].nil?
-                   record.read!(:mother_race_code, identity[:mother][:race_code], force: true) unless identity[:mother][:race_code].nil?
-
-                  #father
-                   record.read!(:father_country_code, identity[:father][:country_code], force: true) unless identity[:father][:country_code].nil?
-                   record.read!(:father_identification_number, identity[:father][:identification_number], force: true) unless identity[:father][:identification_number].nil?
-                   record.read!(:father_race_code, identity[:father][:race_code], force: true) unless identity[:father][:race_code].nil?
-
 
 
                   animal.fetch(:mouvements, []).each do |mvt|
-                    if mvt[:entry]
-                      record.read!(:entry_date, mvt[:entry][:entry_date], at: mvt[:entry][:entry_date], force: true) unless mvt[:entry][:entry_date].nil?
-                      record.read!(:entry_reason, mvt[:entry][:entry_reason], at: mvt[:entry][:entry_date], force: true) unless mvt[:entry][:entry_reason].nil?
+
+                    mvt[:entry] ||= {}
+                    mvt[:exit] ||= {}
+
+                    if mvt[:entry].present?
+                      record.movements.create!(product: record, delta: record.initial_population, started_at: mvt[:entry][:entry_date])
+                      record.initial_movement = record.movements.first
+
+                      record.initial_born_at = mvt[:entry][:entry_date] if record.initial_born_at.blank?
+
+                      record.save!
+
+                      record.read!(:entry_date, mvt[:entry][:entry_date], at: record.born_at, force: true) unless mvt[:entry][:entry_date].nil?
+                      record.read!(:entry_reason, mvt[:entry][:entry_reason], at: record.born_at, force: true) unless mvt[:entry][:entry_reason].nil?
                     end
 
-                    if mvt[:exit]
-                      record.read!(:exit_date, mvt[:entry][:exit_date], at: mvt[:entry][:exit_date], force: true) unless mvt[:entry][:exit_date].nil?
-                      record.read!(:exit_reason, mvt[:entry][:exit_reason], at: mvt[:entry][:exit_date], force: true) unless mvt[:entry][:exit_reason].nil?
+                    if mvt[:exit].present?
+                      record.movements.create!(product: record, delta: -record.initial_population, started_at: mvt[:entry][:exit_date])
+
+                      record.read!(:exit_date, mvt[:entry][:exit_date], at: record.born_at, force: true) unless mvt[:entry][:exit_date].nil?
+                      record.read!(:exit_reason, mvt[:entry][:exit_reason], at: record.born_at, force: true) unless mvt[:entry][:exit_reason].nil?
                     end
                   end
 
+                  {
+                      healthy: true,
+                      witness: identity[:birth_date][:witness],
+                      cpb_filiation_status: identity[:cpb_filiation_status],
+                      birth_date: identity[:birth_date][:date],
+                      birth_farm_number: identity[:farm_number],
+                      first_calving_date: identity[:first_calving_date],
+                      origin_country_code: identity[:origin_country_code],
+                      origin_identification_number: identity[:origin_identification_number],
+                      end_of_life_witness: identity[:end_of_life_witness],
+                      country_code: identity[:country_code],
+                      mother_country_code: identity[:mother][:country_code],
+                      mother_identification_number: identity[:mother][:identification_number],
+                      mother_race_code: identity[:mother][:race_code],
+                      father_country_code: identity[:father][:country_code],
+                      father_identification_number: identity[:father][:identification_number],
+                      father_race_code: identity[:father][:race_code]
+                  }.each do |k, v|
+                    record.read!(k, v, at: record.born_at, force: true) unless v.nil?
+                  end
+
+                rescue => e
+                  #TODO: format errors
+                  binding.pry
+                  logger.state = [identity[:identification_number], e.record.errors.messages].to_s if e.respond_to?(:record)
+                  logger.save!
                 end
-              rescue => e
-                #TODO: format errors
-                binding.pry
-                logger.state = [identity[:identification_number], e.record.errors.messages].to_s if e.respond_to?(:record)
-                logger.save!
               end
             end
 
