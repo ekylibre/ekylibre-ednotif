7ecb83690b85177be1548bd98139e81d3086808f
Nicolas PROCUREUR <nprocureur@gmail.com>
2016-11-10 17:35:29 +0100

    Adds hook to run get inventory

--- config/initializers/ednotif_initializer.rb
+++ config/initializers/ednotif_initializer.rb
@@ -1,14 +1,12 @@
-# autoload won't work on conditional module inclusion
-require 'ekylibre/ednotif'
-
 autoload :Ednotif, 'ednotif'
 
 # Ednotif::EdnotifIntegration.on_check_success do
   # Ednotif::EdnotifGetListJob.perform_later
 # end
 
-#hook : subscribe :new_birth,
-
+Ekylibre::Hook.subscribe :get_inventory do |data|
+  Ednotif::GetInventoryJob.perform_now(data)
+end
 
 # Loads transcoding tables
 files = Dir.glob(Ekylibre::Ednotif.in_transcoding_dir.join('*')) - Dir.glob(Ekylibre::Ednotif.in_transcoding_dir.join('*.exception.yml'))
