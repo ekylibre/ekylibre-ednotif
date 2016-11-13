a3088567ac442ce74c95903ef04d66a68b16e560
Nicolas PROCUREUR <nprocureur@gmail.com>
2016-11-13 17:59:05 +0100

    Clean animals action

--- app/controllers/backend/synchronization_operations_controller.rb
+++ app/controllers/backend/synchronization_operations_controller.rb
@@ -9,6 +9,13 @@ module Backend
       @synchronization_operation = SynchronizationOperation.find(params[:id])
     end
 
+    def clean_animals
+      Animal.find_each do |animal|
+        animal.destroy if animal.destroyable?
+      end
+      redirect_to backend_animals_path
+    end
+
     list order: { created_at: :desc } do |t|
       t.column :created_at
       t.column :operation_name, url: true
