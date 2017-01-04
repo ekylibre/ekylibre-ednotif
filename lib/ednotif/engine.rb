c931b4221657124a8698f39adc8e1990796027d5
Alexandre Lécuelle <alecuelle@ekylibre.com>
2017-01-04 16:28:02 +0100

    Add toolbar and navigations initializers

--- lib/ednotif/engine.rb
+++ lib/ednotif/engine.rb
@@ -7,5 +7,22 @@ module Ednotif
     initializer :i18n do |app|
       app.config.i18n.load_path += Dir[Ednotif::Engine.root.join('config', 'locales', '**', '*.yml')]
     end
+
+    initializer :extend_navigation do |app|
+      Ekylibre::Navigation.exec_dsl do
+        part :production do
+          group :animal_management do
+            item :synchronization_operation do
+              page "backend/synchronization_operations#index"
+              page "backend/synchronization_operations#show"
+            end
+          end
+        end
+      end
+    end
+
+    initializer :extend_toolbar do |app|
+      Ekylibre::View::Addon.add('backend/animals/import_cattling_inventory_toolbar', to: :toolbar, for_action: 'backend/animals#index')
+    end
   end
 end
